6764a94135a640c7db846aa8728465cdd5bd7c5e
Zahra-Nasiri <z.nasir.8181@gmail.com>
2023-09-25 13:07:56 +0330

    Implemented new model name Image.

--- src/djshop/envs/common.py
+++ src/djshop/envs/common.py
@@ -129,6 +129,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_ROOT = "/vol/web/media/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
