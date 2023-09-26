9e81b02b870f4043b37696a52c33ef62b433dde9
Zahra-Nasiri <z.nasir.8181@gmail.com>
2023-09-26 18:29:13 +0330

    Implemented exception to save duplicate images.

--- src/djshop/apps/media/models.py
+++ src/djshop/apps/media/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from django.dispatch import receiver
+from django.db.models.signals import pre_save
 import hashlib
+from .exceptions import DuplicateImageException
 
 # Create your models here.
 class Image(models.Model):
-    title = models.ForeignKey(max_length=128, null=True, blank=True)
+    title = models.CharField(max_length=128, null=True, blank=True)
     image = models.ImageField(width_field='width', height_field='height')
 
     width = models.IntegerField(editable=False)
@@ -24,6 +27,13 @@ class Image(models.Model):
         for chunk in self.image.file.chunks():
             hasher.update(chunk)
 
-        self.file_hash = hasher.digest()
+        self.file_hash = hasher.hexdigest()
 
         super().save(*argws, **kwargs)
+
+
+@receiver(pre_save, sender=Image)
+def check_duplication_hash(sender, instance, **kwargs):
+    existed = Image.objects.filter(file_hash=instance.file_hash).exists()
+    if existed:
+        raise DuplicateImageException("Duplicate")
